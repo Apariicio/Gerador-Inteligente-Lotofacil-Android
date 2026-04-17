cdf2f4e1307d9695ccd58d106c67f251421d55cc
Aparicio V. Amaral <liumanutencao@gmail.com>
2026-04-16 21:07:49 -0300

    Atualizando resultados dos jogos recentes ate a data 15-04-26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.aparicioamaral.quinzenumerosaleatorios"
         minSdk = 29
         targetSdk = 35
-        versionCode = 50
-        versionName = "4.5.9.1"
+        versionCode = 51
+        versionName = "4.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
